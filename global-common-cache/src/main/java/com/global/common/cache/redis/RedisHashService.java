da3f04733ac7eed14f17aa34cc908c4c5e17b97d
鲁砚琨 <yankun.lu@0071515.com>
2020-01-15 18:44:34 +0800

    增加新的redis处理实现类

---
+++ global-common-cache/src/main/java/com/global/common/cache/redis/RedisHashService.java
@@ -0,0 +1,13 @@
+package com.global.common.cache.redis;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 哈希结构缓存处理类
+ * @Author: 鲁砚琨
+ * @CreateTime: 2020-01-14 下午 03:20
+ * @Version: v1.0
+ */
+@Service
+public class RedisHashService extends RedisBasicService {
+}
---
+++ global-common-cache/src/main/java/com/global/common/cache/redis/RedisListService.java
@@ -0,0 +1,13 @@
+package com.global.common.cache.redis;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 列表结构缓存处理类
+ * @Author: 鲁砚琨
+ * @CreateTime: 2020-01-14 下午 03:26
+ * @Version: v1.0
+ */
+@Service
+public class RedisListService extends RedisBasicService {
+}
