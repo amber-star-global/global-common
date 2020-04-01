d43335bef0a931f36983deb656729ed9f9d9f25a
鲁砚琨 <yankun.lu@0071515.com>
2020-04-01 14:59:59 +0800

    增加持久层配置

---
+++ global-common-persistence/global-common-persistence-mybatis/src/main/java/com/global/common/persistence/mybatis/config/MyBatisConfig.java
@@ -0,0 +1,16 @@
+package com.global.common.persistence.mybatis.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: 鲁砚琨
+ * @CreateTime: 2020-03-31 下午 04:53
+ * @Version: v1.0
+ */
+@Slf4j
+@Configuration
+@MapperScan(basePackages = "${mybatis.mapper.scan.package}")
+public class MyBatisConfig {
+}
---
+++ global-common-persistence/src/main/java/com/global/common/persistence/config/DataSourceConfig.java
@@ -0,0 +1,31 @@
+package com.global.common.persistence.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+/**
+ * @Author: 鲁砚琨
+ * @CreateTime: 2020-03-31 下午 04:49
+ * @Version: v1.0
+ */
+@Slf4j
+@Configuration
+@EnableTransactionManagement
+public class DataSourceConfig implements TransactionManagementConfigurer {
+
+    @Autowired
+    private DataSource dataSource;
+
+
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        log.info("配置事务管理, 配置数据源: {}", dataSource);
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
