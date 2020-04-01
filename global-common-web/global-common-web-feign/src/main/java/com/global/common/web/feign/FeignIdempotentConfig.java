db5301323a043e36b76b1c889e1112b0b93aa965
鲁砚琨 <yankun.lu@0071515.com>
2020-04-01 16:24:17 +0800

    增加feign幂等校验处理

---
+++ global-common-web/global-common-web-feign/src/main/java/com/global/common/web/feign/FeignIdempotentConfig.java
@@ -0,0 +1,37 @@
+package com.global.common.web.feign;
+
+import com.global.common.web.idempotent.GlobalIdempotent;
+import com.global.common.web.idempotent.IdempotentUtil;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collection;
+
+/**
+ * @Author: 鲁砚琨
+ * @CreateTime: 2020-04-01 下午 04:19
+ * @Version: v1.0
+ */
+@Slf4j
+@Configuration
+public class FeignIdempotentConfig implements RequestInterceptor {
+
+    @Autowired
+    private IdempotentUtil idempotentUtil;
+
+    @Override
+    public void apply(RequestTemplate template) {
+        // feign接口幂等处理
+        log.info("获取feign接口调用信息; 调用接口: {}, 请求方式: {}", template.url(), template.method());
+        Collection<String> feignTokenHeader = template.headers().get(GlobalIdempotent.REQUEST_IDEM_TOKEN);
+        if (feignTokenHeader != null && feignTokenHeader.size() > 0) {
+            // 当前接口需要实现幂等处理, 插入token值,
+            String value = idempotentUtil.getToken();
+            log.info("添加feign幂等token: {}", value);
+            template.header(GlobalIdempotent.REQUEST_IDEM_TOKEN, value);
+        }
+    }
+}
--- global-common-web/global-common-web-idempotent/src/main/java/com/global/common/web/idempotent/IdempotentUtil.java
+++ global-common-web/global-common-web-idempotent/src/main/java/com/global/common/web/idempotent/IdempotentUtil.java
@@ -7,6 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @Description: 幂等校验处理工具类
  * @Author: 鲁砚琨
@@ -19,6 +22,29 @@ public class IdempotentUtil {
 
     @Autowired
     private RedisValueService redisValueService;
+
+
+    /**
+     * 获取Token方法
+     * @return
+     */
+    public String getToken() {
+        String tokenValue = UUID.randomUUID().toString();
+        setToken(tokenValue);
+        log.info("获取token值: {}", tokenValue);
+        return tokenValue;
+    }
+
+    /**
+     * 设置token值，默认一个token存在10分钟
+     */
+    private void setToken(String key) {
+        // 设置幂等Key,value
+        redisValueService.put(key, null);
+        // 设置过期时间
+        redisValueService.expire(key, 10, TimeUnit.MINUTES);
+    }
+
 
     /**
      * 幂等处理方法
