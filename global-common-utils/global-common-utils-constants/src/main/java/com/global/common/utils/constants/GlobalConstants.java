080eab035cc8ff286919b398276b13bf9aaa2189
鲁砚琨 <yankun.lu@0071515.com>
2020-12-09 16:23:27 +0800

    定义全局常量类

---
+++ global-common-utils/global-common-utils-constants/src/main/java/com/global/common/utils/constants/GlobalConstants.java
@@ -0,0 +1,16 @@
+package com.global.common.utils.constants;
+
+/**
+ *
+ * @Author: 鲁砚琨
+ * @CreateTime: 2020-12-09 下午 03:52
+ * @Version: v1.0
+ */
+public interface GlobalConstants {
+
+    /**
+     * HTTP请求头, 操作人信息KEY
+     */
+    String HEADER_OPERATOR_KEY = "X-REQ-OPERATOR";
+
+}
