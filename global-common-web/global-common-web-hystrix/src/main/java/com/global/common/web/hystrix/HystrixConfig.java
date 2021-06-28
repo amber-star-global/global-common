a9ab85a332a4b9367a9b4aacdec02e3c2e028f0b
鲁砚琨 <yankun.lu@0071515.com>
2021-06-28 16:49:26 +0800

    增加注解描述

--- global-common-web/global-common-web-hystrix/src/main/java/com/global/common/web/hystrix/HystrixConfig.java
+++ global-common-web/global-common-web-hystrix/src/main/java/com/global/common/web/hystrix/HystrixConfig.java
@@ -5,6 +5,10 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * @EnableHystrix: 启用断路器
+ * @EnableHystrixDashboard: 启用可视化组件
+ * http://localhost:9001/hystrix
+ *
  * 配置hystrix详情参照:  https://www.pianshen.com/article/1661146624/
  * @Author: 鲁砚琨
  * @CreateTime: 2021-05-19 下午 01:54
